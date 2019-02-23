405ff21a4fc35b72df52d6c3e49f01f877d994db
kevindul <47910194+kevindul@users.noreply.github.com>
2019-02-23 00:28:36 -0500

    Create Pseudocode.java

---
+++ Pseudocode.java
@@ -0,0 +1,23 @@
+//init functions, namespace
+//class calls to device specific engines
+//sublime or corona work great for cross platform
+
+//using any temp based sensor for phones or prints
+//get (temp);{
+//if temp(standardFingerprintTemp) || if(voiceRecognized) || if(faceRecognized);{
+//{fingerprintScan=1);{
+//elseIf(voiceRecognized);{
+//listenForPassword=1;}
+//elseIf(faceRecognized);{
+//openRetinalScan=1;}
+//}}}
+//if(fingerprintScan=1;){
+//init(scanSeq);
+//{ if (print [0-9] inOrder) && (seqOrder = 1);
+//{unlockDevice=1;}
+//try/catch to handle exceptions for order based lockout,
+//settings for number of tries before lockout 
+//settings before time before more attemps
+//if any prints are recognized as someone elses during, options for
+//how the program reacts with alarm/vibrate or email sent based on device
+}
